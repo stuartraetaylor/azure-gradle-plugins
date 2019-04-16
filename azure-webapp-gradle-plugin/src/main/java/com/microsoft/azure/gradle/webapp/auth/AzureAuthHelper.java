64e54210f7878253779b229bb28f6128a61a6977
aalmiray <aalmiray@gmail.com>
2019-04-16 14:05:45 +0200

    Rework build settings; fix file license headers

--- azure-webapp-gradle-plugin/src/main/java/com/microsoft/azure/gradle/webapp/auth/AzureAuthHelper.java
+++ azure-webapp-gradle-plugin/src/main/java/com/microsoft/azure/gradle/webapp/auth/AzureAuthHelper.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2019 Elena Lakhno
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.microsoft.azure.gradle.webapp.auth;
 
 import com.microsoft.azure.AzureEnvironment;
@@ -90,7 +113,7 @@ public class AzureAuthHelper {
 
     protected Authenticated getAuthObj() {
         Authenticated auth;
-        // check if project has Azure authentication settings in build.gradle
+        // check if project has Azure authentication settings in azure-functions-gradle-plugin.gradle
         // or gradle.properties or in environment variables
         final Authentication authSetting = config.getAuthenticationSettings();
         if (authSetting.getType() == null) {
--- azure-webapp-gradle-plugin/src/main/java/com/microsoft/azure/gradle/webapp/helpers/CommonStringTemplates.java
+++ azure-webapp-gradle-plugin/src/main/java/com/microsoft/azure/gradle/webapp/helpers/CommonStringTemplates.java
@@ -1,11 +1,34 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2019 Elena Lakhno
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.microsoft.azure.gradle.webapp.helpers;
 
 public final class CommonStringTemplates {
-    public static final String PROPERTY_MISSING_TEMPLATE = "'%s' is not configured in build.gradle.";
-    public static final String UNKNOWN_VALUE_TEMPLATE = "Unknown value: '%s' in build.gradle";
+    public static final String PROPERTY_MISSING_TEMPLATE = "'%s' is not configured in azure-functions-gradle-plugin.gradle.";
+    public static final String UNKNOWN_VALUE_TEMPLATE = "Unknown value: '%s' in azure-functions-gradle-plugin.gradle";
     public static final String APP_SERVICE_PROPERTY_MISSING_TEMPLATE =
-            "'%s' is required in build.gradle when appService.type is '%s'";
+            "'%s' is required in azure-functions-gradle-plugin.gradle when appService.type is '%s'";
     public static final String NOT_COMPATIBLE_WEBAPP_TEMPLATE =
-            "azureWebApp configured an existing web app in build.gradle and it is not '%s' Web App. "
+            "azureWebApp configured an existing web app in azure-functions-gradle-plugin.gradle and it is not '%s' Web App. "
                     + "Please align appService configuration with a compatible web app. ";
 }
